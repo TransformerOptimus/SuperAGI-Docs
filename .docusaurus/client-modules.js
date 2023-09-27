6f679e736d8432db54088f4e02f2854e52345fbd
Akshat Jain <jainakshat75@gmail.com>
2023-09-27 16:55:27 +0530

    added nodeJS Client Library

--- .docusaurus/client-modules.js
+++ .docusaurus/client-modules.js
@@ -1,4 +1,5 @@
 export default [
+  require('D:\\SuperAGI-Docs\\node_modules\\@docusaurus\\plugin-google-gtag\\lib\\gtag'),
   require('D:\\SuperAGI-Docs\\node_modules\\infima\\dist\\css\\default\\default.css'),
   require('D:\\SuperAGI-Docs\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
   require('D:\\SuperAGI-Docs\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -3,41 +3,6 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
-    path: '/docs/__docusaurus/debug',
-    component: ComponentCreator('/docs/__docusaurus/debug', 'e00'),
-    exact: true
-  },
-  {
-    path: '/docs/__docusaurus/debug/config',
-    component: ComponentCreator('/docs/__docusaurus/debug/config', '2df'),
-    exact: true
-  },
-  {
-    path: '/docs/__docusaurus/debug/content',
-    component: ComponentCreator('/docs/__docusaurus/debug/content', '695'),
-    exact: true
-  },
-  {
-    path: '/docs/__docusaurus/debug/globalData',
-    component: ComponentCreator('/docs/__docusaurus/debug/globalData', 'd12'),
-    exact: true
-  },
-  {
-    path: '/docs/__docusaurus/debug/metadata',
-    component: ComponentCreator('/docs/__docusaurus/debug/metadata', '9e2'),
-    exact: true
-  },
-  {
-    path: '/docs/__docusaurus/debug/registry',
-    component: ComponentCreator('/docs/__docusaurus/debug/registry', 'b7c'),
-    exact: true
-  },
-  {
-    path: '/docs/__docusaurus/debug/routes',
-    component: ComponentCreator('/docs/__docusaurus/debug/routes', '08f'),
-    exact: true
-  },
-  {
     path: '/docs/markdown-page',
     component: ComponentCreator('/docs/markdown-page', '822'),
     exact: true
